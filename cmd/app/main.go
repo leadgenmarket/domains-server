66e85ca21aa23c6d6f1f39630a4136a94d776228
kanya384 <kanya384@mail.ru>
2021-12-28 00:25:29 +0300

    added autiocert need to check\

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,12 +5,14 @@ import (
 	"domain-server/internal/handlers"
 	"domain-server/internal/logger"
 	"domain-server/internal/repositories"
+	"domain-server/internal/repositories/domains"
 	"domain-server/internal/services"
-	"fmt"
 
+	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 	mongo "github.com/globalsign/mgo"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -26,9 +28,31 @@ func main() {
 		logger.GetInstance().Panic("error initializing config: %w", err)
 	}
 	repo := repositories.New(sess.DB("leadgen"), cfg)
+	domainsList, err := GetAllDomainUrls(repo.Domains)
+	if err != nil {
+		logger.GetInstance().Panic("error initializing config: %w", err)
+	}
+
+	m := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(domainsList...),
+		Cache:      autocert.DirCache("./certs"),
+	}
 	router := gin.Default()
 	servicesContainer := services.Setup(cfg)
 	handlersService := handlers.New(router, repo, servicesContainer, logger)
 	handlersService.Registry()
-	router.Run(fmt.Sprintf(":%s", cfg.Port))
+	logger.GetInstance().Panic(autotls.RunWithManager(router, &m))
+}
+
+func GetAllDomainUrls(repository domains.Repository) ([]string, error) {
+	domains := []string{}
+	listDomain, err := repository.GetAllDomains()
+	if err != nil {
+		return nil, err
+	}
+	for _, domain := range listDomain {
+		domains = append(domains, domain.Url)
+	}
+	return domains, nil
 }
--- go.mod
+++ go.mod
@@ -13,6 +13,12 @@ require (
 )
 
 require (
+	github.com/gin-gonic/autotls v0.0.3 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
@@ -22,12 +28,12 @@ require (
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
