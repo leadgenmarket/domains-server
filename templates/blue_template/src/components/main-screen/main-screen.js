aacb3c39e3682a5dd94074ebceafef4617a17d77
kanya384 <kanya384@mail.ru>
2022-01-11 17:09:13 +0300

    added sub_title param to template

--- templates/blue_template/src/components/main-screen/main-screen.js
+++ templates/blue_template/src/components/main-screen/main-screen.js
@@ -1,6 +1,7 @@
 import React from "react"
 import BtnComponent from "../btn-component"
 import CityTitle from "../city-title"
+import parse from 'html-react-parser'
 
 const MainScreen = ({params, nextStep}) => {
     return <React.Fragment>
@@ -10,7 +11,7 @@ const MainScreen = ({params, nextStep}) => {
                         <div className="wmain">
                             <CityTitle params={params} />
                             <h1>{params.title==""?"Недорогие новостройки":params.title}</h1>
-                            <div className="sub_title">Здесь собрана полная база квартир, более 30 000 вариантов. <br />За 1 минуту вы получите бесплатный доступ к самым выгодным предложениям!</div>
+                            <div className="sub_title">{params.sub_title === undefined || params.sub_title==""?parse("Здесь собрана полная база квартир, более 30 000 вариантов. <br />За 1 минуту вы получите бесплатный доступ к самым выгодным предложениям!"):parse(params.sub_title)}</div>
                             <BtnComponent text={"Начать быстрый поиск"} params={params} clickFunct={nextStep}/>
                         </div>
                     </div>
