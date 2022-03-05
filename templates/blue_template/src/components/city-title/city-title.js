6c66a998bfa1962307184bee47324ac4cbd8b4a0
kanya384 <kanya384@mail.ru>
2022-03-05 17:44:14 +0300

    test

--- templates/blue_template/src/components/city-title/city-title.js
+++ templates/blue_template/src/components/city-title/city-title.js
@@ -1,8 +1,8 @@
 import LocationPin from "../location-pin"
 
-const CityTitle = ({params, index, length}) => {
+const CityTitle = ({ params, index, length }) => {
     return (
-        <div className="city_title" style={{color:`#${params.main_color}`, display:"flex", fontSize:"28px", paddingLeft:0}}><LocationPin fill={"#"+params.main_color} /> {domainSettings.city.Name} {index!=null?<span>Шаг {index+1}/{length}</span>:""}</div>
+        <div className="city_title" style={{ color: `#${params.main_color}`, display: "flex", fontSize: "28px", paddingLeft: 0 }}><LocationPin fill={"#" + params.main_color} /> {domainSettings.city.Name} {index != null ? <span>Шаг {domainSettings.rayon === "" ? index + 1 : index}/{domainSettings.rayon !== "" ? length - 1 : length}</span> : ""}</div>
     )
 }
 
--- templates/blue_template/src/components/steps/step.js
+++ templates/blue_template/src/components/steps/step.js
@@ -5,18 +5,33 @@ import SliderStep from "./slider-step";
 import TextStep from "./text-step";
 import RoomsStep from "./rooms-step";
 
-const Step = ({ step, params, index, length, nextStep, prevStep, raionsStep, roomsStep, form, setForm }) => {
+const Step = ({ step, params, index, length, nextStep, prevStep, raionsStep, roomsStep, form, setForm, ignoreFirst }) => {
+
+    const backBtn = (index, ignoreFirst) => {
+        switch (index) {
+            case 0:
+                return ""
+            case 1:
+                if (ignoreFirst) {
+                    return ""
+                } else {
+                    return <BtnComponent text={"Назад"} params={params} clickFunct={prevStep} />
+                }
+            default:
+                return <BtnComponent text={"Назад"} params={params} clickFunct={prevStep} />
+        }
+    }
     return (<div style={{ display: "block" }} className="page_main pages ">
         <div className="page">
             <div className="page_inner">
                 <div className="wmain">
                     <CityTitle params={params} index={index} length={length} />
                     <div className="title_inner title_room">{step.title}</div>
-                    {step.type == "text" || step.type == "raions" || step.type == "sdacha" ? <TextStep step ={step} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm = {setForm} /> :step.type == "rooms"?<RoomsStep step ={step} params={params} index={index} length={length} raionsStep={raionsStep} nextStep={nextStep} prevStep={prevStep} form={form} setForm = {setForm} />:<SliderStep step ={step} raionsStep={raionsStep} roomsStep={roomsStep} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm = {setForm} />}
-                     <div className="btn_block">
-                        {index > 0 ? <BtnComponent text={"Назад"} params={params} clickFunct={prevStep}/> : ""}
-                        <BtnComponent text={"Дальше"} params={params} clickFunct={nextStep}/>
-                      </div>
+                    {step.type == "text" || step.type == "raions" || step.type == "sdacha" ? <TextStep step={step} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm={setForm} /> : step.type == "rooms" ? <RoomsStep step={step} params={params} index={index} length={length} raionsStep={raionsStep} nextStep={nextStep} prevStep={prevStep} form={form} setForm={setForm} /> : <SliderStep step={step} raionsStep={raionsStep} roomsStep={roomsStep} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm={setForm} />}
+                    <div className="btn_block">
+                        {backBtn(index, ignoreFirst)}
+                        <BtnComponent text={"Дальше"} params={params} clickFunct={nextStep} />
+                    </div>
                 </div>
             </div>
         </div>
