0efbc42e396c1c618744cbdb0001a111177bb6f5
kanya384 <kanya384@mail.ru>
2022-03-19 16:14:20 +0300

    /

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -77,6 +77,7 @@ func (h *handlers) Registry() {
 	h.router.Static("/templates", templatesFolder)
 	h.router.Static("/moderation", moderationFolder)
 	h.router.Static("/admin", "./admin/build")
+	h.router.Static("/plans_admin", "./plans_admin/build")
 	h.router.Static("/file-store", "./file-store")
 	h.router.LoadHTMLFiles("./admin/build/index.html", "./plans_admin/build/plans_admin.html", "./templates/plans_template/build/plans_template.html", "./templates/blue_template/build/blue_template.html", "./templates/wa_template/build/wa_template.html", "./templates/purple_template/build/purple_template.html", "./templates/test/build/test.html", "./moderation/template/moderation_1.html", "./moderation/template2/moderation_2.html", "./moderation/krd/krd.html", "./moderation/spb/spb.html")
 	h.router.GET("/", h.Domains.GetTemplate)
