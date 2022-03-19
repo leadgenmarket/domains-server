33ef3e0baac2ce31ee0682dd39c7186c021b755c
kanya384 <kanya384@mail.ru>
2022-03-19 16:13:21 +0300

    added plans_admin to templates

--- plans_admin/src/optimize.js
+++ plans_admin/src/optimize.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
 fs.readFile('./build/index.html', 'utf8', (err, data) => {
-  data = data.replaceAll('"/css', '"/plans_admin/css')
-  data = data.replaceAll('"/static', '"/plans_admin/static')
-  data = data.replace('href="favicon.ico"', 'href="/admin/favicon.ico"')
+  data = data.replaceAll('"/css', '"/plans_admin/build/css')
+  data = data.replaceAll('"/static', '"/plans_admin/build/static')
+  data = data.replace('href="favicon.ico"', 'href="/plans_admin/build/favicon.ico"')
   //data = data.replace('</body>', '<script> let domainSettings = JSON.parse("{{ .scripts }}")</script></body>')
   fs.unlink("./build/index.html", () => { })
   fs.writeFileSync("./build/plans_admin.html", data)
