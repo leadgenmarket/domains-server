4e470d57f0cd780607cdf425afb03731c21c49d8
kanya384 <kanya384@mail.ru>
2022-04-06 15:06:15 +0300

    blue_template added sub title

--- templates/blue_template/src/components/main-screen/main-screen.js
+++ templates/blue_template/src/components/main-screen/main-screen.js
@@ -3,21 +3,27 @@ import BtnComponent from "../btn-component"
 import CityTitle from "../city-title"
 import parse from 'html-react-parser'
 
-const MainScreen = ({params, nextStep}) => {
+const MainScreen = ({ params, nextStep }) => {
     return <React.Fragment>
-                <div style={{display: "block"}} className="page_main">
-                <div className="page page_1">
-                    <div className="page_inner">
-                        <div className="wmain">
-                            <CityTitle params={params} />
-                            <h1>{params.title==""?"Недорогие новостройки":params.title}</h1>
-                            <div className="sub_title">{params.sub_title === undefined || params.sub_title==""?parse("Здесь собрана полная база квартир, более 30 000 вариантов. <br />За 1 минуту вы получите бесплатный доступ к самым выгодным предложениям!"):parse(params.sub_title)}</div>
-                            <BtnComponent text={"Начать быстрый поиск"} params={params} clickFunct={nextStep}/>
+        <div style={{ display: "block" }} className="page_main">
+            <div className="page page_1">
+                <div className="page_inner">
+                    <div className="wmain">
+                        <CityTitle params={params} />
+                        <h1>{params.title == "" ? "Недорогие новостройки" : params.title}</h1>
+                        <div className="sub_title">{params.sub_title === undefined || params.sub_title == "" ? parse("Здесь собрана полная база квартир, более 30 000 вариантов. <br />За 1 минуту вы получите бесплатный доступ к самым выгодным предложениям!") : parse(params.sub_title)}
+                            {domainSettings.domain.sub_title_items === undefined || domainSettings.domain.sub_title_items == '' ? "" :
+                                <React.Fragment>
+                                    <br />
+                                    <ul class="header_list">{parse(domainSettings.domain.sub_title_items)}</ul>
+                                </React.Fragment>}
                         </div>
+                        <BtnComponent text={"Начать быстрый поиск"} params={params} clickFunct={nextStep} />
                     </div>
                 </div>
             </div>
-        </React.Fragment>
+        </div>
+    </React.Fragment>
 }
 
 export default MainScreen
