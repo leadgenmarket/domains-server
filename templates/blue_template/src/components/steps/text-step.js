4f471200e53f3c2ea1401ad5ed9c3ceafad9dc88
kanya384 <kanya384@mail.ru>
2022-04-08 12:11:14 +0300

    changed scrollbar in blue

--- templates/blue_template/src/components/steps/text-step.js
+++ templates/blue_template/src/components/steps/text-step.js
@@ -1,11 +1,13 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import CheckBoxItem from "./checkbox-item"
 import ColoredScrollbars from "../colored-scrollbars/colored-scrollbars";
+import SimpleBarReact from "simplebar-react";
+import "simplebar/src/simplebar.css";
 
 const TextStep = ({ step, params, index, length, nextStep, prevStep, form, setForm }) => {
     const [raionsAvailable, setAvailableRaions] = useState([])
     const amIChecked = (answer) => {
-       return form[step.title].split(", ").includes(answer)
+        return form[step.title].split(", ").includes(answer)
     }
 
     const updateCheck = (answer) => {
@@ -18,14 +20,14 @@ const TextStep = ({ step, params, index, length, nextStep, prevStep, form, setFo
 
     const setAnswer = (answerIn) => {
         let answers = form[step.title].split(", ")
-       
+
         if (answers[0] == "") {
-            answers.splice(0,1)
+            answers.splice(0, 1)
         }
         answers.push(answerIn)
-       
-        
-        
+
+
+
         setForm((prev) => {
             let newForm = {}
             for (let i in prev) {
@@ -35,11 +37,11 @@ const TextStep = ({ step, params, index, length, nextStep, prevStep, form, setFo
             return newForm
         })
     }
-        
+
     const unsetAnswer = (answerOut) => {
         let answers = form[step.title].split(", ")
         if (answers[0] == "") {
-            answers.splice(0,1)
+            answers.splice(0, 1)
         }
         for (var key in answers) {
             if (answers[key] === answerOut) {
@@ -64,14 +66,14 @@ const TextStep = ({ step, params, index, length, nextStep, prevStep, form, setFo
         } else {
             unsetAllRaions()
         }
-    } 
+    }
 
     const setAllRaions = () => {
         let newForm = {}
         for (let i in form) {
             newForm[i] = form[i]
         }
-        newForm[step.title] = "Все, "+domainSettings.locations.map((location) => location.NameFull).join(", ")
+        newForm[step.title] = "Все, " + domainSettings.locations.map((location) => location.NameFull).join(", ")
         setForm(newForm)
     }
 
@@ -86,18 +88,18 @@ const TextStep = ({ step, params, index, length, nextStep, prevStep, form, setFo
 
 
     if (step.type == "raions") {
-        return <ColoredScrollbars style={{ backgroundColor:"transparent", width: "100%", height: 300 }}>
-                <ul className="check_list_one check_list" id="kv_list">
-                    <CheckBoxItem key={"Все"}  checked={amIChecked("Все")} updateCheck={updateCheckAll} answer={"Все"} step ={step} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm = {setForm} /> 
-                    {domainSettings.locations.map((location) => {
-                        return <CheckBoxItem key={location.NameFull} checked={amIChecked(location.NameFull)} updateCheck={updateCheck} answer={location.NameFull} step ={step} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm = {setForm} />
-                    })}
-                </ul>
-            </ColoredScrollbars>
+        return <SimpleBarReact style={{ maxHeight: 300 }} autoHide={false}>
+            <ul className="check_list_one check_list" id="kv_list">
+                <CheckBoxItem key={"Все"} checked={amIChecked("Все")} updateCheck={updateCheckAll} answer={"Все"} step={step} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm={setForm} />
+                {domainSettings.locations.map((location) => {
+                    return <CheckBoxItem key={location.NameFull} checked={amIChecked(location.NameFull)} updateCheck={updateCheck} answer={location.NameFull} step={step} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm={setForm} />
+                })}
+            </ul>
+        </SimpleBarReact>
     }
     return <ul className="check_list_one check_list" id="kv_list">
         {step.answers.map((answer) => {
-            return <CheckBoxItem checked={amIChecked(answer)} updateCheck={updateCheck} key={answer} answer={answer} step ={step} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm = {setForm} />
+            return <CheckBoxItem checked={amIChecked(answer)} updateCheck={updateCheck} key={answer} answer={answer} step={step} params={params} index={index} length={length} nextStep={nextStep} prevStep={prevStep} form={form} setForm={setForm} />
         })}
     </ul>
 }
